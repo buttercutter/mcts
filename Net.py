d729664574f64d0da06385950204f94bb33bd21f
promach <feiphung@hotmail.com>
2020-12-27 23:59:14 +0800

    Solved a training code bug where the dataset are being overwritten after the first epoch

--- Net.py
+++ Net.py
@@ -20,12 +20,11 @@ num_of_board_features_and_turn = num_of_board_features + 1  # add 1 because of p
 # Input is all the board features (2x9 squares) plus the turn.
 board_features_and_turn = df.iloc[:, list(range(num_of_board_features)) + [player_turn]]
 
-num_of_possible_moves = 9   # a normal 3x3 tic-tac-toe has 9 input boxes
+num_of_possible_moves = 9  # a normal 3x3 tic-tac-toe has 9 input boxes
 num_of_possible_scores = 3  # -1, 0, 1 == loss, draw, win
 
 # Target variables are the possible move squares.
-moves = df.iloc[:, list(range(num_of_board_features, num_of_board_features+num_of_possible_moves))]
-
+moves = df.iloc[:, list(range(num_of_board_features, num_of_board_features + num_of_possible_moves))]
 
 # To predict score instead, use this as the target:
 score = pd.get_dummies(df['score'])
@@ -69,60 +68,64 @@ class Net(nn.Module):
         )
 
     def forward(self, x):
-
         policy = self.linears(x)
         value = self.linears2(x)
 
         return policy, value  # score, move
 
 
-net = Net()
-print(net)
+def train():
+    net = Net()
+    print(net)
 
-params = list(net.parameters())
-print(len(params))
+    params = list(net.parameters())
+    print(len(params))
 
-# for x in range(10):
-#    print(params[x].size())  # conv1's .weight
+    # for x in range(10):
+    #    print(params[x].size())  # conv1's .weight
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-lossFunction = nn.MSELoss()
+    # loss_function = nn.CrossEntropyLoss()
+    loss_function = nn.MSELoss()
 
-num_epochs = 500
-for epoch in range(num_epochs):
-    loss_ = 0
+    num_epochs = 100
+    for epoch in range(num_epochs):
+        loss_ = 0
 
-    # See https://github.com/bsamseth/tictacNET/issues/2
-    # for a description of the inputs and output of the neural network
-    train_loader = list(zip(
-        torch.from_numpy(np.array(board_features_and_turn, dtype='float32')[:, np.newaxis]),
-        torch.from_numpy(np.array(moves, dtype='float32')[:, np.newaxis]),
-        torch.from_numpy(np.array(score, dtype='float32')[:, np.newaxis]),
-    ))
+        # See https://github.com/bsamseth/tictacNET/issues/2
+        # for a description of the inputs and output of the neural network
+        train_loader = list(zip(
+            torch.from_numpy(np.array(board_features_and_turn, dtype='float32')[:, np.newaxis]),
+            torch.from_numpy(np.array(moves, dtype='float32')[:, np.newaxis]),
+            torch.from_numpy(np.array(score, dtype='float32')[:, np.newaxis]),
+        ))
 
-    for board_features_and_turn, moves, score in train_loader:
-        # Forward Pass
-        policy_output, value_output = net(board_features_and_turn)
-        # Loss at each iteration by comparing to target(moves)
-        loss1 = lossFunction(policy_output, moves)
-        # Loss at each iteration by comparing to target(score)
-        loss2 = lossFunction(value_output, score)
+        for _board_features_and_turn, move, _score in train_loader:
+            # Forward Pass
+            policy_output, value_output = net(_board_features_and_turn)
+            # Loss at each iteration by comparing to target(moves)
+            loss1 = loss_function(policy_output, move)
+            # Loss at each iteration by comparing to target(score)
+            loss2 = loss_function(value_output, _score)
 
-        loss = loss1 + loss2
+            loss = loss1 + loss2
 
-        # Backpropogating gradient of loss
-        optimizer.zero_grad()
-        loss.backward()
+            # Backpropogating gradient of loss
+            optimizer.zero_grad()
+            loss.backward()
 
-        # Updating parameters(weights and bias)
-        optimizer.step()
+            # Updating parameters(weights and bias)
+            optimizer.step()
 
-        loss_ += loss.item()
-    print("Epoch{}, Training loss:{}".format(epoch, loss_ / len(score)))
+            loss_ += loss.item()
+        print("Epoch{}, Training loss:{}".format(epoch, loss_ / len(score)))
 
-print('Finished Training')
+    print('Finished Training')
 
-PATH = './tictactoe_net.pth'
-torch.save(net, PATH)
+    path = './tictactoe_net.pth'
+    torch.save(net, path)
+
+
+if __name__ == "__main__":
+    train()
