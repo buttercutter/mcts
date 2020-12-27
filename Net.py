2a827dc3434af710c5c4cbdec7d3a9c659176b09
promach <feiphung@hotmail.com>
2020-12-27 16:32:00 +0800

    Combined both policy network (move) and value network (score) into a single NN

--- Net.py
+++ Net.py
@@ -15,14 +15,16 @@ print("Scores:", Counter(df["score"]))
 
 num_of_board_features = 18
 player_turn = -2  # second to last column of the csv file
+num_of_board_features_and_turn = num_of_board_features + 1  # add 1 because of player turn
 
 # Input is all the board features (2x9 squares) plus the turn.
 board_features_and_turn = df.iloc[:, list(range(num_of_board_features)) + [player_turn]]
 
-num_of_possible_moves = 9
+num_of_possible_moves = 9   # a normal 3x3 tic-tac-toe has 9 input boxes
+num_of_possible_scores = 3  # -1, 0, 1 == loss, draw, win
 
 # Target variables are the possible move squares.
-# moves = df.iloc[:, list(range(num_of_board_features, num_of_board_features+num_of_possible_moves))]
+moves = df.iloc[:, list(range(num_of_board_features, num_of_board_features+num_of_possible_moves))]
 
 
 # To predict score instead, use this as the target:
@@ -35,7 +37,7 @@ class Net(nn.Module):
         super(Net, self).__init__()
 
         self.linears = nn.Sequential(
-            nn.Linear(19, 128),
+            nn.Linear(num_of_board_features_and_turn, 128),
             nn.ReLU(),
             nn.Dropout(),
 
@@ -47,13 +49,31 @@ class Net(nn.Module):
             nn.ReLU(),
             nn.Dropout(),
 
-            nn.Linear(32, 3)
+            nn.Linear(32, num_of_possible_moves)
+        )
+
+        self.linears2 = nn.Sequential(
+            nn.Linear(num_of_board_features_and_turn, 128),
+            nn.ReLU(),
+            nn.Dropout(),
+
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(),
+
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Dropout(),
+
+            nn.Linear(32, num_of_possible_scores)
         )
 
     def forward(self, x):
 
-        out = self.linears(x)
-        return out
+        policy = self.linears(x)
+        value = self.linears2(x)
+
+        return policy, value  # score, move
 
 
 net = Net()
@@ -70,7 +90,7 @@ optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
 lossFunction = nn.MSELoss()
 
-num_epochs = 100
+num_epochs = 500
 for epoch in range(num_epochs):
     loss_ = 0
 
@@ -78,14 +98,19 @@ for epoch in range(num_epochs):
     # for a description of the inputs and output of the neural network
     train_loader = list(zip(
         torch.from_numpy(np.array(board_features_and_turn, dtype='float32')[:, np.newaxis]),
+        torch.from_numpy(np.array(moves, dtype='float32')[:, np.newaxis]),
         torch.from_numpy(np.array(score, dtype='float32')[:, np.newaxis]),
     ))
 
-    for board_features_and_turn, score in train_loader:
+    for board_features_and_turn, moves, score in train_loader:
         # Forward Pass
-        output = net(board_features_and_turn)
+        policy_output, value_output = net(board_features_and_turn)
+        # Loss at each iteration by comparing to target(moves)
+        loss1 = lossFunction(policy_output, moves)
         # Loss at each iteration by comparing to target(score)
-        loss = lossFunction(output, score)
+        loss2 = lossFunction(value_output, score)
+
+        loss = loss1 + loss2
 
         # Backpropogating gradient of loss
         optimizer.zero_grad()
